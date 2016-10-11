a6255fe72f6a0667d8e2b93406eb4ef7517d899b
Mark Miyashita <negativetwelve@gmail.com>
2016-10-11 15:08:47 -0700

    Add iOS files for the native bridge. Start with setupWithAppToken method

---
+++ ios/RNLookback.h
@@ -0,0 +1,15 @@
+//
+//  RNLookback.h
+//  RNLookback
+//
+//  Created by Mark Miyashita on 10/11/16.
+//  Copyright © 2016 Mark Miyashita. All rights reserved.
+//
+
+
+#import "RCTBridgeModule.h"
+#import "RCTUtils.h"
+
+
+@interface RNLookback : NSObject <RCTBridgeModule>
+@end
---
+++ ios/RNLookback.m
@@ -0,0 +1,22 @@
+//
+//  RNLookback.m
+//  RNLookback
+//
+//  Created by Mark Miyashita on 10/11/16.
+//  Copyright © 2016 Mark Miyashita. All rights reserved.
+//
+
+
+#import "RNLookback.h"
+#import <Lookback/Lookback.h>
+
+
+@implementation RNLookback
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(startWithAppToken:(NSString *)appToken) {
+  [LookBack setupWithAppToken:appToken];
+}
+
+@end
